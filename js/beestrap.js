581ad49bc0741f03f33dec343cc43b8b03367e94
davide <davide.dantonio@webeetle.com>
2018-11-16 17:26:34 +0100

    fix sidebar

---
+++ js/beestrap.js
@@ -0,0 +1,203 @@
+$(document).on('click', '.navbar-toggle', function() {
+  $toggle = $(this);
+
+  if (blackDashboard.misc.navbar_menu_visible == 1) {
+    $('html').removeClass('nav-open');
+    blackDashboard.misc.navbar_menu_visible = 0;
+    setTimeout(function() {
+      $toggle.removeClass('toggled');
+      $('#bodyClick').remove();
+    }, 550);
+
+  } else {
+    setTimeout(function() {
+      $toggle.addClass('toggled');
+    }, 580);
+
+    div = '<div id="bodyClick"></div>';
+    $(div).appendTo('body').click(function() {
+      $('html').removeClass('nav-open');
+      blackDashboard.misc.navbar_menu_visible = 0;
+      setTimeout(function() {
+        $toggle.removeClass('toggled');
+        $('#bodyClick').remove();
+      }, 550);
+    });
+
+    $('html').addClass('nav-open');
+    blackDashboard.misc.navbar_menu_visible = 1;
+  }
+});
+
+blackDashboard = {
+  misc: {
+    navbar_menu_visible: 0
+  },
+
+  checkScrollForTransparentNavbar: debounce(function() {
+    if ($(document).scrollTop() > scroll_distance) {
+      if (transparent) {
+        transparent = false;
+        $('.navbar[color-on-scroll]').removeClass('navbar-transparent');
+      }
+    } else {
+      if (!transparent) {
+        transparent = true;
+        $('.navbar[color-on-scroll]').addClass('navbar-transparent');
+      }
+    }
+  }, 17),
+
+
+
+  // Activate DateTimePicker
+
+  initDateTimePicker: function() {
+    if ($(".datetimepicker").length != 0) {
+      $('.datetimepicker').datetimepicker({
+        icons: {
+          time: "tim-icons icon-watch-time",
+          date: "tim-icons icon-calendar-60",
+          up: "fa fa-chevron-up",
+          down: "fa fa-chevron-down",
+          previous: 'tim-icons icon-minimal-left',
+          next: 'tim-icons icon-minimal-right',
+          today: 'fa fa-screenshot',
+          clear: 'fa fa-trash',
+          close: 'fa fa-remove'
+        }
+      });
+    }
+
+    if ($(".datepicker").length != 0) {
+      $('.datepicker').datetimepicker({
+        format: 'MM/DD/YYYY',
+        icons: {
+          time: "tim-icons icon-watch-time",
+          date: "tim-icons icon-calendar-60",
+          up: "fa fa-chevron-up",
+          down: "fa fa-chevron-down",
+          previous: 'tim-icons icon-minimal-left',
+          next: 'tim-icons icon-minimal-right',
+          today: 'fa fa-screenshot',
+          clear: 'fa fa-trash',
+          close: 'fa fa-remove'
+        }
+      });
+    }
+
+    if ($(".timepicker").length != 0) {
+      $('.timepicker').datetimepicker({
+        // format: 'H:mm',    // use this format if you want the 24hours timepicker
+        format: 'h:mm A', //use this format if you want the 12hours timpiecker with AM/PM toggle
+        icons: {
+          time: "tim-icons icon-watch-time",
+          date: "tim-icons icon-calendar-60",
+          up: "fa fa-chevron-up",
+          down: "fa fa-chevron-down",
+          previous: 'tim-icons icon-minimal-left',
+          next: 'tim-icons icon-minimal-right',
+          today: 'fa fa-screenshot',
+          clear: 'fa fa-trash',
+          close: 'fa fa-remove'
+        }
+      });
+    }
+  },
+
+  initMinimizeSidebar: function() {
+    if ($('.sidebar-mini').length != 0) {
+      sidebar_mini_active = true;
+    }
+
+    $('.minimize-sidebar').click(function() {
+      var $btn = $(this);
+
+      if (sidebar_mini_active == true) {
+        $('body').removeClass('sidebar-mini');
+        sidebar_mini_active = false;
+        blackDashboard.showSidebarMessage('Sidebar mini deactivated...');
+      } else {
+        $('body').addClass('sidebar-mini');
+        sidebar_mini_active = true;
+        blackDashboard.showSidebarMessage('Sidebar mini activated...');
+      }
+
+      // we simulate the window Resize so the charts will get updated in realtime.
+      var simulateWindowResize = setInterval(function() {
+        window.dispatchEvent(new Event('resize'));
+      }, 180);
+
+      // we stop the simulation of Window Resize after the animations are completed
+      setTimeout(function() {
+        clearInterval(simulateWindowResize);
+      }, 1000);
+    });
+  },
+
+  startAnimationForLineChart: function(chart) {
+    chart.on('draw', function(data) {
+      if (data.type === 'line' || data.type === 'area') {
+        data.element.animate({
+          d: {
+            begin: 600,
+            dur: 700,
+            from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+            to: data.path.clone().stringify(),
+            easing: Chartist.Svg.Easing.easeOutQuint
+          }
+        });
+      } else if (data.type === 'point') {
+        seq++;
+        data.element.animate({
+          opacity: {
+            begin: seq * delays,
+            dur: durations,
+            from: 0,
+            to: 1,
+            easing: 'ease'
+          }
+        });
+      }
+    });
+
+    seq = 0;
+  },
+  startAnimationForBarChart: function(chart) {
+
+    chart.on('draw', function(data) {
+      if (data.type === 'bar') {
+        seq2++;
+        data.element.animate({
+          opacity: {
+            begin: seq2 * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: 'ease'
+          }
+        });
+      }
+    });
+
+    seq2 = 0;
+  },
+  showSidebarMessage: function(message) {
+    try {
+      $.notify({
+        icon: "tim-icons icon-bell-55",
+        message: message
+      }, {
+        type: 'primary',
+        timer: 4000,
+        placement: {
+          from: 'top',
+          align: 'right'
+        }
+      });
+    } catch (e) {
+      console.log('Notify library is missing, please make sure you have the notifications library added.');
+    }
+
+  }
+};
